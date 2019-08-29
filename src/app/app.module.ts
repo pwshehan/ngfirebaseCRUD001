b4b447facf7b41aeba782836e4e2fe9ffc3e1826
Shehan_Pramitha <pwshehan@gmail.com>
2019-08-29 19:24:57 +0530

    Almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,24 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { CustomerDetailsComponent } from './customers/customer-details/customer-details.component';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
+import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerDetailsComponent,
+    CustomersListComponent,
+    CreateCustomerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
